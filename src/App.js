b712fbf8879a0ad4e23bf1e02e968852bd42ee02
Mya-Mya <interleukin413@gmail.com>
2022-03-24 00:06:46 +0900

    prepare components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Container, Grid, Stack } from "@mui/material";
 import { RecoilRoot } from "recoil";
+import WindPreviewView from "./components/WindPreviewView";
+import TrailsSettingView from "./components/TrailsSettingView";
+import LevelRangeView from "./components/LevelRangeView";
 const App = () => {
   return (
     <RecoilRoot>
+      <Container>
+        <Stack spacing={1} pt={1}>
+          <WindPreviewView />
+          <Grid container spacing={1}>
+            <Grid item sm={6}>
+              <TrailsSettingView />
+            </Grid>
+            <Grid item sm={6}>
+              <LevelRangeView />
+            </Grid>
+          </Grid>
+        </Stack>
+      </Container>
     </RecoilRoot>
   );
 };
