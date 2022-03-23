5f2fa51110e31fa2ec5be181de6e3a8d42340f0c
Mya-Mya <interleukin413@gmail.com>
2022-03-24 00:03:36 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
+const App = () => {
+  return (
+    <RecoilRoot>
+    </RecoilRoot>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
