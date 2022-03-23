2c42d836d6fa9d8f008af2b139bedc29e6adc3a1
Mya-Mya <interleukin413@gmail.com>
2022-03-23 14:40:31 +0900

    add Levels setting view

--- doms.js
+++ doms.js
@@ -1,17 +1,35 @@
 var doms = {
-    NumberOfTrailsDOM: document.getElementById("number-of-trails"),
-    onAutomaticallyCreateTrailsChange:function(handler){
-        var dom = document.getElementById("automatically-create-trails");
-        dom.onchange = function(){
-            handler(dom.checked)
-        }
-        dom.onchange()
-    },
-    onNumberOfTrailsGoalChange:function(handler){
-        var dom = document.getElementById("number-of-trails-goal");
-        dom.onchange = function(){
-            handler(dom.value)
-        }
-        dom.onchange()
-    }
-}
+  NumberOfTrailsDOM: document.getElementById("number-of-trails"),
+  onAutomaticallyCreateTrailsChange: function (handler) {
+    var dom = document.getElementById("automatically-create-trails");
+    dom.onchange = function () {
+      handler(dom.checked);
+    };
+    dom.onchange();
+  },
+  onNumberOfTrailsGoalChange: function (handler) {
+    var dom = document.getElementById("number-of-trails-goal");
+    dom.onchange = function () {
+      handler(dom.value);
+    };
+    dom.onchange();
+  },
+  /**
+   *
+   * @param {(number,number)=>void} handler
+   */
+  onLevelsChange: function (handler) {
+    var minLevelDOM = document.getElementById("min-level");
+    var levelSpaceDOM = document.getElementById("level-space");
+    var levelRangeDOM = document.getElementById("level-range");
+    var oninput = function () {
+      var minLevel = float(minLevelDOM.value);
+      var maxLevel = minLevel + float(levelSpaceDOM.value);
+      levelRangeDOM.innerText = minLevel + " ~ " + maxLevel;
+      handler(minLevel, maxLevel);
+    };
+    minLevelDOM.oninput = oninput;
+    levelSpaceDOM.oninput = oninput;
+    oninput();
+  },
+};
