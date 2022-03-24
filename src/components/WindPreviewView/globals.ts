6374b40c3e4390c2d8e38ead7930fd307dace6e5
Mya-Mya <interleukin413@gmail.com>
2022-03-24 22:54:27 +0900

    WindPreviewView js->ts

--- src/components/WindPreviewView/globals.ts
+++ src/components/WindPreviewView/globals.ts
@@ -1,8 +1,18 @@
 import { P5Instance } from "react-p5-wrapper";
 import Transformer from "../../models/Transformer";
-import { WindPreviewViewProps } from "./index";
 import { Complex } from "complex.js";
 import ParticleStyle from "../../models/particlestyle/ParticleStyle";
+type WindPreviewViewGlobals = {
+  p: P5Instance;
+  transformer: Transformer;
+  particleStyle: ParticleStyle;
+  func: (z: Complex) => Complex;
+  leftU: number;
+  rightU: number;
+  topV: number;
+  bottomV: number;
+  dispatch: (x: any) => void;
+};
 export default {
   /**@type {P5Instance} */ p: null,
   /**@type {Transformer} */ transformer: null,
@@ -13,4 +23,4 @@ export default {
   /**@type {number} */ topV: 0,
   /**@type {number} */ bottomV: 0,
   /**@type {any=>void} */ dispatch: null,
-};
+} as WindPreviewViewGlobals;
