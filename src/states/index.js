15b718b0839c6d82dfd14d18f8bfd0743c9e181a
Mya-Mya <interleukin413@gmail.com>
2022-03-24 14:52:40 +0900

    create settings slice

--- src/states/index.js
+++ src/states/index.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import trailsReducer from "./trailsSlice";
 import levelReducer from "./levelSlice";
 import c2csampleReducer from "./c2csampleSlice";
+import settingsReducer from "./settingsSlice";
 export const store = configureStore({
   reducer: {
     trails: trailsReducer,
     level: levelReducer,
     c2csample: c2csampleReducer,
+    settings: settingsReducer,
   },
 });
