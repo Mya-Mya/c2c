4438e6583b46b916126e68c107bbd01e92bb69c7
Mya-Mya <interleukin413@gmail.com>
2022-03-24 14:51:31 +0900

    add cscsample select view

--- src/states/index.js
+++ src/states/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import trailsReducer from "./trailsSlice";
 import levelReducer from "./levelSlice";
+import c2csampleReducer from "./c2csampleSlice";
 export const store = configureStore({
   reducer: {
     trails: trailsReducer,
     level: levelReducer,
+    c2csample: c2csampleReducer,
   },
 });
