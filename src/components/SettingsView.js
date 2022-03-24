d37c0ecd022837b3ac78153b575a3358864ee209
Mya-Mya <interleukin413@gmail.com>
2022-03-24 14:53:24 +0900

    composite setting views

---
+++ src/components/SettingsView.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Container, Tabs, Tab } from "@mui/material";
+import C2cSampleSelectView from "./C2cSampleSelectView";
+import TrailsSettingView from "./TrailsSettingView";
+import LevelRangeView from "./LevelRangeView";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectSettingsState,
+  setSettingViewName,
+  SETTING_VIEW_NAMES,
+} from "../states/settingsSlice";
+const SettingView = ({ openingSettingViewName }) => {
+  switch (openingSettingViewName) {
+    case SETTING_VIEW_NAMES.C2C_SAMPLE_SELECT:
+      return <C2cSampleSelectView />;
+    case SETTING_VIEW_NAMES.LEVEL_RANGE:
+      return <LevelRangeView />;
+    case SETTING_VIEW_NAMES.TRAILS_SETTING:
+      return <TrailsSettingView />;
+    default:
+      return <C2cSampleSelectView />;
+  }
+};
+
+export default () => {
+  const dispatch = useDispatch();
+  const openingSettingViewName =
+    useSelector(selectSettingsState).settingViewName;
+  return (
+    <Container>
+      <Tabs
+        value={openingSettingViewName}
+        onChange={(e, v) => dispatch(setSettingViewName(v))}
+      >
+        <Tab label="C2C Sample" value={SETTING_VIEW_NAMES.C2C_SAMPLE_SELECT} />
+        <Tab label="Trails Setting" value={SETTING_VIEW_NAMES.TRAILS_SETTING} />
+        <Tab label="Level Range" value={SETTING_VIEW_NAMES.LEVEL_RANGE} />
+      </Tabs>
+      <SettingView openingSettingViewName={openingSettingViewName} />
+    </Container>
+  );
+};
